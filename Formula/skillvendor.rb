8ddf4e3ea164c983f196df628e726fec9adbd6cf
mattjmcnaughton <me@mattjmcnaughton.com>
2026-06-25 23:15:54 +0000

    chore(skillvendor): update formula to v1.1.0

--- Formula/skillvendor.rb
+++ Formula/skillvendor.rb
@@ -1,30 +1,30 @@
 class Skillvendor < Formula
   desc "Vendor remote skills from git repositories into local skill directories"
   homepage "https://github.com/mattjmcnaughton/skillvendor"
-  version "1.0.1"
+  version "1.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.0.1/skillvendor-macos-arm64.tar.gz"
-      sha256 "935f6d54e632d85d760772d97fbd83ddc9f2f7e5aa21d8f94a46a08d45a69bb7"
+      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.1.0/skillvendor-macos-arm64.tar.gz"
+      sha256 "942b1f17ea45dd846b4aebfed07163ec63559732b9afa99d2490bc7be3854c97"
     end
 
     on_intel do
-      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.0.1/skillvendor-macos-x86_64.tar.gz"
-      sha256 "c726ff2a37ba49dd1251756e93b39273a1a21ba490ff7936e1824f10dbe48d51"
+      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.1.0/skillvendor-macos-x86_64.tar.gz"
+      sha256 "7dac1ad96fe58a912264de673ccaa0f562ac39a689420a7a8b9a983eb3539b00"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.0.1/skillvendor-linux-arm64.tar.gz"
-      sha256 "49c41664993fe757849eeb1c7e5770aded4ccb11a46cbbdd0d13ade2804399b4"
+      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.1.0/skillvendor-linux-arm64.tar.gz"
+      sha256 "a27484f84d8d6af90601c8fe26b2155f1efc9a8481c039b8660b95e3b6e22318"
     end
 
     on_intel do
-      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.0.1/skillvendor-linux-x86_64.tar.gz"
-      sha256 "e7bed68db80b078f4c573c57fc7b5499a00f9810da2c474bcdb9ad957b6b09cb"
+      url "https://github.com/mattjmcnaughton/skillvendor/releases/download/v1.1.0/skillvendor-linux-x86_64.tar.gz"
+      sha256 "0d4a643c022d8aecf8f45eea76564b21e638f42f6d87256bbe4e36e97089ed13"
     end
   end
 
